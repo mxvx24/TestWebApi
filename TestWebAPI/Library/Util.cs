9681abd78cdb9a4ed52a744222591639e5fb842f
Mohammed <tuniis@live.com>
2019-09-27 21:15:33 -0500

    Revert "Test: Update local."
    This reverts commit 794c041b964221fa301cfe84df14abfdc86aba01.

--- TestWebAPI/Library/Util.cs
+++ TestWebAPI/Library/Util.cs
@@ -6,7 +6,7 @@
     using Newtonsoft.Json;
 
     /// <summary>
-    /// The utility class. Test: Update local.
+    /// The utility class. Test.
     /// </summary>
     public static class Util
     {
