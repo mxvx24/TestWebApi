90a48838b96780090f1dc8247f5c500207f5a803
Mohammed <tuniis@live.com>
2019-11-01 22:18:32 -0500

    Added new users controller to call RandomUserApi using HttpClient

---
+++ TestWebAPI/DTOs/RandomUserApi/User.cs
@@ -0,0 +1,28 @@
+﻿namespace TestWebAPI.DTOs
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Threading.Tasks;
+
+    /// <summary>
+    /// The user.
+    /// </summary>
+    public class User
+    {
+        /// <summary>
+        /// Gets or sets the name.
+        /// </summary>
+        public Name Name { get; set; }
+
+        /// <summary>
+        /// Gets or sets the gender.
+        /// </summary>
+        public string Gender { get; set; }
+
+        /// <summary>
+        /// Gets or sets the location.
+        /// </summary>
+        public Location Location { get; set; }
+    }
+}
