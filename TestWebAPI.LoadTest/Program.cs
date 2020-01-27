305471fb8fafb25c0776a4430c8596462d204587
Mohammed Hoque <mhoque@elevate.com>
2020-01-27 10:03:09 -0600

    Added test comment

--- TestWebAPI.LoadTest/Program.cs
+++ TestWebAPI.LoadTest/Program.cs
@@ -31,6 +31,7 @@
         /// </summary>
         static Program()
         {
+            // Test
             HttpClient = new HttpClient() { BaseAddress = new Uri("http://localhost:5000") };
             HttpClient.DefaultRequestHeaders.Clear();
             HttpClient.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue(MediaTypeNames.Application.Json));
